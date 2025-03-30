a344bf412c140ad5c28c80af4d6ffdd32617fc83
Marcos <marcos.ram.ort@gmail.com>
2025-03-30 18:40:35 -0300

    Adicion de boton ClickMe con Bootstrap, Adicion de Framework

--- InterfazWebReact/src/App.jsx
+++ InterfazWebReact/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from 'react-bootstrap';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      {/* Botón nuevo con BootstrapMarcos */}
+      <div className="container mt-5">
+        <h1>¡Hola, Bootstrap en Vite!</h1>
+        <Button variant="primary">Click me</Button>
+      </div>
     </>
   )
 }
