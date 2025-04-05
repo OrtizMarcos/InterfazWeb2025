1784b564a29aeab4dcb807ffa20e144367e3937d
JackDelvalle <126831977+Skexz019@users.noreply.github.com>
2025-04-05 20:25:46 -0300

    cambios

--- InterfazWebReact/src/layouts/NavbarComponent.jsx
+++ InterfazWebReact/src/layouts/NavbarComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import './styles/Navbar.css';
+import '../styles/Navbar.css';
 
 function NavbarComponent() {
   return (
--- InterfazWebReact/src/main.jsx
+++ InterfazWebReact/src/main.jsx
@@ -1,8 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 createRoot(document.getElementById('root')).render(
