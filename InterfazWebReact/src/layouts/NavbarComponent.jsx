9e4976d0df7cdcf65c540364a2339bac86f7e96c
JackDelvalle <126831977+Skexz019@users.noreply.github.com>
2025-04-06 14:47:08 -0300

    Agregada barra de búsqueda y carrito, centrados enlaces de navegación

--- InterfazWebReact/src/layouts/NavbarComponent.jsx
+++ InterfazWebReact/src/layouts/NavbarComponent.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, Form, Button } from 'react-bootstrap';
+import { FaShoppingCart, FaSearch } from 'react-icons/fa';
 import "../styles/Navbar.css";
 
 function NavbarComponent() {
   return (
     <Navbar expand="lg" className="custom-navbar" sticky="top" variant="dark">
-      <div className="navbar-content">
+      <Container fluid>
         <Navbar.Brand href="/" className="navbar-brand-text">
           JAM
         </Navbar.Brand>
+        <Form className="d-flex search-form">
+          <Form.Control
+            type="search"
+            placeholder="Buscar productos..."
+            className="me-2 search-input"
+            aria-label="Search"
+          />
+          <Button variant="outline-light" className="search-button">
+            <FaSearch />
+          </Button>
+        </Form>
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="navbar-toggle" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto nav-links">
@@ -17,8 +29,11 @@ function NavbarComponent() {
             <Nav.Link href="/Ubicaciones">Ubicaciones</Nav.Link>
             <Nav.Link href="/contactos">Contacto</Nav.Link>
           </Nav>
+          <Nav.Link href="/carrito" className="cart-icon">
+            <FaShoppingCart size={24} />
+          </Nav.Link>
         </Navbar.Collapse>
-      </div>
+      </Container>
     </Navbar>
   );
 }
