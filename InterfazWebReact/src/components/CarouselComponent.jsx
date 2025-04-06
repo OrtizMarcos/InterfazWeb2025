f2a53542bcbe0ed902ca582c4c935a49dc1aeec5
Marcos <marcos.ram.ort@gmail.com>
2025-04-06 14:15:40 -0300

    Cambio de diseño en imagenes

--- InterfazWebReact/src/components/CarouselComponent.jsx
+++ InterfazWebReact/src/components/CarouselComponent.jsx
@@ -1,15 +1,15 @@
 import Carousel from 'react-bootstrap/Carousel';
-import Digital1 from '../img/Digital1.jpg';
-import digital4 from '../img/digital4.jpg';
-import digital3 from '../img/digital3.jpg';
-import digital2 from '../img/digital2.jpg';
+import AtencionCliente from '../img/AtencionCliente.jpg';
+import Delivery from '../img/Delivery.jpg';
+import Electro1 from '../img/Electro1.jpg';
+import Instalacion from '../img/Instalacion.jpg';
 import '../styles/Carousel.css';
 
 function CarouselComponent() {
   return (
     <Carousel fade interval={3000} controls indicators>
       <Carousel.Item>
-        <img className="d-block w-100" src={Digital1} alt="JBL TUNE 510BT" style={{ maxHeight: '400px', objectFit: 'cover' }} />
+        <img className="d-block w-100" src={Electro1} alt="JBL TUNE 510BT" style={{ maxHeight: '400px', objectFit: 'cover' }} />
         <Carousel.Caption>
           <h5>JBL TUNE 510BT</h5>
           <p>Auriculares inalámbricos on-ear con tecnología Bluetooth.</p>
@@ -17,7 +17,7 @@ function CarouselComponent() {
       </Carousel.Item>
 
       <Carousel.Item>
-        <img className="d-block w-100" src={digital2} alt="Logitech G203" style={{ maxHeight: '400px', objectFit: 'cover' }} />
+        <img className="d-block w-100" src={Delivery} alt="Logitech G203" style={{ maxHeight: '400px', objectFit: 'cover' }} />
         <Carousel.Caption>
           <h5>Gaming Mouse Logitech G203</h5>
           <p>Ratón Gaming LightSync blanco WHT</p>
@@ -25,7 +25,7 @@ function CarouselComponent() {
       </Carousel.Item>
 
       <Carousel.Item>
-        <img className="d-block w-100" src={digital3} alt="VIOTEK Monitor" style={{ maxHeight: '400px', objectFit: 'cover' }} />
+        <img className="d-block w-100" src={AtencionCliente} alt="VIOTEK Monitor" style={{ maxHeight: '400px', objectFit: 'cover' }} />
         <Carousel.Caption>
           <h5>VIOTEK SUW49C Monitor Curvo</h5>
           <p>Monitor Gaming Curvo</p>
@@ -33,7 +33,7 @@ function CarouselComponent() {
       </Carousel.Item>
 
       <Carousel.Item>
-        <img className="d-block w-100" src={digital4} alt="STOGA Mini Keyboard" style={{ maxHeight: '400px', objectFit: 'cover' }} />
+        <img className="d-block w-100" src={Instalacion} alt="STOGA Mini Keyboard" style={{ maxHeight: '400px', objectFit: 'cover' }} />
         <Carousel.Caption>
           <h5>Teclado Gaming STOGA</h5>
           <p>Miniteclado con iluminación negra arcoíris.</p>
